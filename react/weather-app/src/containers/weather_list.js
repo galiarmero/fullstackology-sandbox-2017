e94d0c067a544de8cf78000679735d24148b8bc5
garmero <galileo.armero@nokia.com>
2017-09-03 18:20:17 +0800

    Show sparklines chart for 5 day weather forecast data

--- react/weather-app/src/containers/weather_list.js
+++ react/weather-app/src/containers/weather_list.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; 
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const cityName = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
         
         return (
             <tr key={ cityName }>
                 <td>{ cityName }</td>
+                <td><Chart data={temps} color='#00ff19' units='K' /></td>
+                <td><Chart data={pressures} color='#00bbff' units='hPa' /></td>
+                <td><Chart data={humidities} color='#ffd400' units="%" /></td>
             </tr>
         );
     }
@@ -17,10 +24,10 @@ class WeatherList extends Component {
             <table className="table table-hover">
                 <thead>
                     <tr>
-                        <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th style={ { width: '25%' } }>City</th>
+                        <th style={ { width: '25%' } }>Temperature (K)</th>
+                        <th style={ { width: '25%' } }>Pressure (hPa)</th>
+                        <th style={ { width: '25%' } }>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
